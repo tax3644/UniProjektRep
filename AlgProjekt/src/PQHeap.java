80e21d1d9ba648b39ac8a38de89054be5781af87
Tax3644 <tandersen3644@hotmail.com>
2020-03-14 22:04:18 +0100

    ggwp

--- AlgProjekt/src/PQHeap.java
+++ AlgProjekt/src/PQHeap.java
@@ -1,65 +1,23 @@
 
-
 import java.util.ArrayList;
+import java.util.List;
+
+/*
+Navne:
+Tobias Albers Andersen
+Jann Jacobsen
+
+SDU-login:
+toand17@student.sdu.dk
+jajac18@student.sdu.dk
+ */
 
 public class PQHeap implements PQ {
 
-    ArrayList<Element> heapArray;
+    private List<Element> heapArray;
 
-    //Constructor of empty ArrayList
     PQHeap () {
         this.heapArray = new ArrayList<>();
-    }
-
-
-
-    public static void main(String[] args) {
-
-        PQHeap heap1 = new PQHeap();
-
-        Element elementm1 = new Element(2, "Integer");
-        Element elementm2 = new Element(2, "Integer");
-        Element elementm3 = new Element(2, "Integer");
-        Element elementm4 = new Element(2, "Integer");
-        Element elementm5 = new Element(2, "Integer");
-        Element elementm6 = new Element(2, "Integer");
-        Element elementm7 = new Element(2, "Integer");
-        Element elementm8 = new Element(2, "Integer");
-        Element elementm9 = new Element(2, "Integer");
-        Element elementm10 = new Element(2, "Integer");
-        Element elementm11 = new Element(2, "Integer");
-        Element elementm12 = new Element(2, "Integer");
-        Element elementm13 = new Element(2, "Integer");
-        Element elementm14 = new Element(2, "Integer");
-
-        heap1.insert(elementm1);
-        heap1.insert(elementm2);
-        heap1.insert(elementm3);
-        heap1.insert(elementm4);
-        heap1.insert(elementm5);
-        heap1.insert(elementm6);
-        heap1.insert(elementm7);
-        heap1.insert(elementm8);
-        heap1.insert(elementm9);
-        heap1.insert(elementm10);
-        heap1.insert(elementm11);
-        heap1.insert(elementm12);
-        heap1.insert(elementm13);
-        heap1.insert(elementm14);
-
-
-        heap1.minHeapifyMachine();
-
-        Element lowestElement;
-        int counter = 0;
-
-        while (heap1.heapArray.size()>0){
-            lowestElement = heap1.extractMin();
-            counter++;
-            System.out.println(counter + " - Element is: " + lowestElement.getKey());
-        }
-
-        System.out.println("lol");
     }
 
     private void minHeapifyMachine(){
@@ -86,13 +44,11 @@ public class PQHeap implements PQ {
         }
     }
 
-    private int left(int i){
-        return i * 2 + 1;
-    }
+    private int left(int i){ return i * 2 + 1; }
     private int right(int i) {
         return i * 2 + 2;
     }
-    public int parent(int i){
+    private int parent(int i){
         if (i % 2 == 1){
             return (i - 1) / 2;
         }
@@ -102,7 +58,6 @@ public class PQHeap implements PQ {
         heapArray.set(indexOne, elementTwo);
         heapArray.set(indexTwo, elementOne);
     }
-
 
     @Override
     public Element extractMin() {
@@ -123,7 +78,5 @@ public class PQHeap implements PQ {
             exchange(heapArray.get(i), heapArray.get(parent(i)), i, parent(i));
             i = parent(i);
         }
-
-
     }
 }
