8d27bc8a2ef8ab88be275cdf56a0a08dcff6f558
janndavid <jann-david77@live.dk>
2020-03-15 16:18:51 +0100

    Code cleanup and added a .gitignore file

--- AlgProjekt/src/PQHeap.java
+++ AlgProjekt/src/PQHeap.java
@@ -16,51 +16,60 @@ public class PQHeap implements PQ {
 
     private List<Element> heapArray;
 
-    PQHeap () {
+    public PQHeap () {
         this.heapArray = new ArrayList<>();
     }
 
     private void minHeapifyMachine(){
-        for (int i = heapArray.size() / 2; i >= 0; i--) {
-            minHeapify(i);
-        }
+        int size = heapArray.size() / 2;
+        for (int i = size; i >= 0; i--) minHeapify(i);
     }
+
     private void minHeapify(int i) {
 
         int left = left(i);
         int right = right(i);
         int smallest;
 
-        if (left<=heapArray.size() - 1 && heapArray.get(left).getKey() < heapArray.get(i).getKey()){
+        if (left <= heapArray.size() - 1 && heapArray.get(left).getKey() < heapArray.get(i).getKey()){
             smallest = left;
-        } else { smallest = i; }
+        } else {
+            smallest = i;
+        }
+
         if (right <= heapArray.size() - 1 && heapArray.get(right).getKey() < heapArray.get(smallest).getKey()) {
             smallest = right;
         }
-        if (smallest != i) {
 
+        if (smallest != i) {
             exchange(heapArray.get(i), heapArray.get(smallest), i, smallest);
             minHeapify(smallest);
         }
     }
 
-    private int left(int i){ return i * 2 + 1; }
+    private int left(int i) {
+        return i * 2 + 1;
+    }
+
     private int right(int i) {
         return i * 2 + 2;
     }
-    private int parent(int i){
-        if (i % 2 == 1){
+
+    private int parent(int i) {
+        if (i % 2 == 1) {
             return (i - 1) / 2;
-        }
+        } else
+
         return (i - 2) / 2;
     }
+
     private void exchange(Element elementOne, Element elementTwo, int indexOne, int indexTwo){
         heapArray.set(indexOne, elementTwo);
         heapArray.set(indexTwo, elementOne);
     }
 
     @Override
-    public Element extractMin() {
+    private Element extractMin() {
         Element min = heapArray.get(0);
         heapArray.set(0, heapArray.get(heapArray.size()-1));
         heapArray.remove(heapArray.size()-1);
@@ -69,7 +78,7 @@ public class PQHeap implements PQ {
     }
 
     @Override
-    public void insert(Element e) {
+    private void insert(Element e) {
 
         heapArray.add(e);
         int i = heapArray.size()-1;
