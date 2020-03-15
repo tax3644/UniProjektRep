d34ce5341729e332c723c47435aeef25c233a045
janndavid <jann-david77@live.dk>
2020-03-15 20:51:30 +0100

    Additional cleaning of code. Also, added a few extra comments to satisfy the conditions of the assignment

--- AlgProjekt/src/PQHeap.java
+++ AlgProjekt/src/PQHeap.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,54 +12,57 @@ jajac18@student.sdu.dk
 */
 
 // Kommentering efter principperne i Software Engineering, Frank Tsui m.fl., 2018
-// Dependencies: PQ interface, klassen Element, List og ArrayList.
+// Dependencies: PQ interfacet, klassen Element, List og ArrayList.
 
 public class PQHeap implements PQ {
 
     /*
-    * PQHeap er en prioritets-kø, "priority queue", som implementerer interfacet PQ samt dets abstrakte
+    * PQHeap er en klasse med en prioritets-kø, "priority queue", som implementerer interfacet PQ samt dets abstrakte
     * metoder: extractMin() og insert(Element e). PQHeap tilbyder herudover kerne- og hjælpemetoder
     * til at minimum heapify instansvariablen 'heapArray'. Metoderne er private da der ingen relevante koblinger er.
-    * med undtagelse af extractMin() og insert(). Ingen teknisk gæld. Implementeret rekursivt.
+    * med undtagelse af extractMin() og insert(). Ingen teknisk gæld, da hjælpemetodernes funktion er at
+    * forbedre læseligheden. Implementeret rekursivt gennem 'minHeapify(int i).
     */
 
+    // heapArrayet er den egentlige kø, det er dog vigtigt at den fyldes med objekter af typen 'Element'
+    // da det interface som vi har implementeret er afhængig af det gennem dets metoder
     private List<Element> heapArray;
 
     public PQHeap () {
         this.heapArray = new ArrayList<>();
     }
 
-    // Driveren af koden som looper igennem 'heapArray' og kalder på minHeapify(i),
-    private void minHeapifyMachine() {
+    // Overloaded metode som looper igennem 'heapArray' og kalder på navnebroren 'minHeapify(i)',
+    private void minHeapify() {
         int size = heapArray.size() / 2;
         for (int i = size; i >= 0; i--) minHeapify(i);
     }
 
-    // minHeapify() har til intention at tjekke at et givet index' indhold skal flyttes ud fra
-    // 'key', som er et medlem i Element-objektet, og som betegner prioriteten som et heltal
+    // 'minHeapify(i)' har til intention at tjekke at et givet index' indhold skal flyttes ud fra
+    // dets 'key', som er et medlem af 'Element'-objektet, og som betegner prioriteten i heltal
     private void minHeapify(int i) {
 
-        // left(i) og right(i) er hjælpemetoder som returnerer de respektive indexer
+        // 'left(i)' og 'right(i)' er hjælpemetoder som returnerer de respektive indexer
         int left = left(i);
         int right = right(i);
         int smallest;
 
         // Sætter den lokale variabel 'smallest' til enten 'left' eller 'i' afhængig af
-        // om left's key er er mindre end i's key eller ej; samt tjekker for indexoutofbound
+        // om "left's" 'key' er er mindre end "i's" 'key' eller ej; samt tjekker for 'indexoutofbound'
         if (left <= heapArray.size() - 1 && heapArray.get(left).getKey() < heapArray.get(i).getKey()){
             smallest = left;
         } else {
             smallest = i;
         }
 
-        // For at tjekke om 'right' har en mindre key kalder vi på getKey() og sammenligner
+        // For at tjekke om 'right' har en mindre 'key' kalder vi på 'getKey()' og sammenligner
         // og sætter den lokale variabel smallest til 'right'
         if (right <= heapArray.size() - 1 && heapArray.get(right).getKey() < heapArray.get(smallest).getKey()) {
             smallest = right;
         }
 
-        // Kalder på hjælpemetoden exchange() og bytter om på de respektive elementer
-        // for at kalde på minHeapify() rekursivt.
+        // Kalder på hjælpemetoden 'exchange()' og bytter om på de respektive elementer
+        // for at kalde på 'minHeapify()' rekursivt.
         if (smallest != i) {
             exchange(heapArray.get(i), heapArray.get(smallest), i, smallest);
             minHeapify(smallest);
@@ -99,12 +101,13 @@ public class PQHeap implements PQ {
         heapArray.set(0, heapArray.get(heapArray.size()-1));
         heapArray.remove(heapArray.size()-1);
         minHeapify(0);
+
         return min;
     }
 
     // For at indsætte et element tilføjer vi det først til 'heapArray' hvilket gør at det kommer
     // sidst i arrayet, vi ved dog nødvendigvis ikke om det har højeste prioritet, derfor må vi
-    // tjekke det og flytte det vha. while-løkken.
+    // tjekke det og muligvis flytte det vha. while-løkken ud fra dets 'key'.
 
     @Override
     public void insert(Element e) {
