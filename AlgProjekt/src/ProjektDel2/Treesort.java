7c0bdacbe097428a7f803167cff7b3fd61a6a88a
Tobias Andersen <tandersen3644@hotmail.com>
2020-04-07 11:12:17 +0200

    kk

--- AlgProjekt/src/ProjektDel2/Treesort.java
+++ AlgProjekt/src/ProjektDel2/Treesort.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 public class Treesort {
     public static void main(String[] args) {
 
+        //Laver et tomt nyt dictionary kaldet dbt
         DictBinTree dbt = new DictBinTree();
         int n = 0;
 
+        //Henter inputtet fra scanneren
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextInt()) {
             dbt.insert(sc.nextInt());
             n++;
         }
+
+        //printer alle integers i arrayet
         for (Integer integersInArray : dbt.orderedTraversal()){
             System.out.println(integersInArray);
         }
